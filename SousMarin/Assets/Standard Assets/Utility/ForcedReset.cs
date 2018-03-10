2f7a1edb51e8b4c4afc1edf74d2178370283f525
Unknown <cylia.adda@gmail.com>
2018-03-10 02:10:26 -0500

    commit project setting

---
+++ SousMarin/Assets/Standard Assets/Utility/ForcedReset.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityStandardAssets.CrossPlatformInput;
+
+[RequireComponent(typeof (GUITexture))]
+public class ForcedReset : MonoBehaviour
+{
+    private void Update()
+    {
+        // if we have forced a reset ...
+        if (CrossPlatformInputManager.GetButtonDown("ResetObject"))
+        {
+            //... reload the scene
+            SceneManager.LoadScene(SceneManager.GetSceneAt(0).name);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
